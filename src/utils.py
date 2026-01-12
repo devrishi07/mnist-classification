1841943af54d174b355574d33e414496521963b2
Rishi <ruthvikgudasani@gmail.com>
2026-01-12 14:29:38 +0530

    Finalizing

--- src/utils.py
+++ src/utils.py
@@ -9,6 +9,7 @@ from sklearn.datasets import fetch_openml
 
 mnist = fetch_openml('mnist_784', as_frame=False)
 
+
 X, y = mnist.data, mnist.target
 
 X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
